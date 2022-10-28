819ca21c640617156146bdde1753434e7f7a1aa2
jangwonjun <jwj51720@gmail.com>
2022-10-28 05:12:45 +0000

    ADD datadirectory

--- wonjun/main.py
+++ wonjun/main.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     arg = parser.add_argument
 
     ############### BASIC OPTION
-    arg('--DATA_PATH', type=str, default='data/', help='Data path를 설정할 수 있습니다.')
+    arg('--DATA_PATH', type=str, default='../main_code/data/', help='Data path를 설정할 수 있습니다.')
     arg('--MODEL', type=str, choices=['FM', 'FFM', 'NCF', 'WDN', 'DCN', 'CNN_FM', 'DeepCoNN'],
                                 help='학습 및 예측할 모델을 선택할 수 있습니다.')
     arg('--DATA_SHUFFLE', type=bool, default=True, help='데이터 셔플 여부를 조정할 수 있습니다.')
