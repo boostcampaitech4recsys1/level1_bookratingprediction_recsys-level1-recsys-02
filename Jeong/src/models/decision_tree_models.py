5b0050d0b51b6c3e308452e013f806875ca8a454
JeongJunHwan <zxxzx1515@naver.com>
2022-10-31 12:05:18 +0000

    UPDATE decision_tree_models.py runnable

--- Jeong/src/models/decision_tree_models.py
+++ Jeong/src/models/decision_tree_models.py
@@ -19,32 +19,46 @@ class DecisionTreeModel:
 
         self.train_dataloader = data["train_dataloader"]
         self.valid_dataloader = data["valid_dataloader"]
-        self.field_dims = data["filed_dims"]
+        self.field_dims = data["field_dims"]
 
-        self.x_train = self.train_dataloader[0]
-        self.y_train = self.train_dataloader[1]
-
-        self.x_valid = self.valid_dataloader[0]
-        self.y_valid = self.valid_dataloader[1]
-
-        self.embed_dim = args.FM_EMBED_DIM
-        self.epochs = args.EPOCHS
+        self.iters = args.CATBOOST_ITERS
         self.learning_rate = args.LR
-        self.weight_decay = args.WEIGHT_DECAY
+        self.depth = args.CATBOOST_DEPTH
         self.log_interval = 100
 
         self.device = args.DEVICE
 
         self.catboost_regressor = CatBoostRegressor(
-            iterations=self.epochs, learning_rate=self.learning_rate, verbose=True
+            iterations=self.iters,
+            learning_rate=self.learning_rate,
+            verbose=True,
+            depth=self.depth,
         )
 
     def train(self):
-        self.catboost_regressor.fit(X=self.x_train, y=self.y_train)
+        for i, (x_train, y_train) in enumerate(tqdm(self.train_dataloader)):
+            x_train = x_train.numpy()
+            y_train = y_train.numpy()
+            self.catboost_regressor.fit(X=x_train, y=y_train, verbose=False)
+            # print(f"valid loss : {self.predict_train()}")
+        print(f"valid loss : {self.predict_train()}")
 
     def predict_train(self):
-        y_hat = self.catboost_regressor.predict(data=self.x_valid)
-        return rmse(self.y_valid, y_hat)
+        y = list()
+        y_hat = list()
+        for i, (x_valid, y_valid) in enumerate(self.valid_dataloader):
+            x_valid = x_valid.numpy()
+            y_valid = y_valid.numpy()
+            y.extend(y_valid)
+            y_hat.extend(self.catboost_regressor.predict(data=x_valid))
+        return rmse(y, y_hat)
 
     def predict(self, test_dataloader):
-        return self.catboost_regressor.predict(data=test_dataloader)
+        predicts = list()
+        for i, x_test_batch in enumerate(tqdm(test_dataloader)):
+
+            for x_test in x_test_batch:
+                x_test = x_test.numpy()
+                temp = self.catboost_regressor.predict(data=x_test)
+                predicts.extend(temp)
+        return predicts
