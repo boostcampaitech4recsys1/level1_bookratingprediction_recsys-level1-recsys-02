c2bf0d9f2a68e7c38e20f353cd3fb38e5bd93c96
JeongJunHwan <zxxzx1515@naver.com>
2022-10-31 02:05:19 +0000

    ADD decision_tree_data.py for decision_tree_models.py

---
+++ Jeong/src/data/decision_tree_data.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader, Dataset
