fe24ec852984df6b11686ba9dce4d5ebebb5c2bd
JeongJunHwan <zxxzx1515@naver.com>
2022-11-01 04:45:21 +0000

    UPDATE dl_data.py to contain context

--- Jeong/src/data/dl_data.py
+++ Jeong/src/data/dl_data.py
@@ -6,6 +6,74 @@ import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader, Dataset
 
 
+def process_dl_data(
+    users: pd.DataFrame,
+    books: pd.DataFrame,
+    rating_train: pd.DataFrame,
+    rating_test: pd.DataFrame,
+):
+    ratings = pd.concat([rating_train, rating_test]).reset_index(drop=True)
+
+    # user에서 활용할 데이터 목록
+    user_cols = ["user_id", "age", "location_state", "location_country"]
+
+    # book에서 활용할 데이터 목록
+    book_cols = [
+        "isbn",
+        "book_author",
+        "year_of_publication",
+        "publisher",
+        "category_high",
+        "isbn_country",
+    ]
+
+    # 활용할 context 목록
+    context_cols = user_cols.copy()
+    context_cols.extend(book_cols)
+    context_cols.remove("user_id")
+    context_cols.remove("isbn")
+
+    # 인덱싱 처리된 데이터 조인
+    context_df = ratings.merge(users[user_cols], on="user_id", how="left").merge(
+        books[book_cols],
+        on="isbn",
+        how="left",
+    )
+    train_df = rating_train.merge(users[user_cols], on="user_id", how="left").merge(
+        books[book_cols],
+        on="isbn",
+        how="left",
+    )
+    test_df = rating_test.merge(users[user_cols], on="user_id", how="left").merge(
+        books[book_cols],
+        on="isbn",
+        how="left",
+    )
+
+    def col2idx(
+        context_df: pd.DataFrame,
+        col: str,
+        train_df: pd.DataFrame,
+        test_df: pd.DataFrame,
+    ):
+        """
+        context df의 col에 대해서 idx화 해주기.
+        """
+        idx_dict = {v: k for k, v in enumerate(context_df[col].unique())}
+        train_df[col] = train_df[col].map(idx_dict)
+        test_df[col] = test_df[col].map(idx_dict)
+        return idx_dict, train_df, test_df
+
+    idx = dict()
+    # 인덱싱 처리
+    for col in context_cols:
+        idx[f"{col}2idx"], train_df, test_df = col2idx(
+            context_df, col, train_df, test_df
+        )
+
+    return idx, train_df, test_df
+
+
 def dl_data_load(args):
 
     ######################## DATA LOAD
@@ -27,16 +95,23 @@ def dl_data_load(args):
     train["user_id"] = train["user_id"].map(user2idx)
     sub["user_id"] = sub["user_id"].map(user2idx)
     test["user_id"] = test["user_id"].map(user2idx)
+    users["user_id"] = users["user_id"].map(user2idx)
 
     train["isbn"] = train["isbn"].map(isbn2idx)
     sub["isbn"] = sub["isbn"].map(isbn2idx)
     test["isbn"] = test["isbn"].map(isbn2idx)
+    books["isbn"] = books["isbn"].map(isbn2idx)
 
-    field_dims = np.array([len(user2idx), len(isbn2idx)], dtype=np.uint32)
+    idx, context_train, context_test = process_dl_data(users, books, train, test)
+    field_dims_list = [len(user2idx), len(isbn2idx)]
+    for k, v in idx.items():
+        field_dims_list.append(len(v))
+
+    field_dims = np.array(field_dims_list, dtype=np.uint32)
 
     data = {
-        "train": train,
-        "test": test.drop(["rating"], axis=1),
+        "train": context_train,
+        "test": context_test.drop(["rating"], axis=1),
         "field_dims": field_dims,
         "users": users,
         "books": books,
