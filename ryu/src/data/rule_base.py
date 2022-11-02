592e63739d4a7c2d30b359ee02365107cc44265d
ryubright <tmxkdd5790@gmail.com>
2022-11-02 18:12:49 +0900

    FIX rule_base

--- ryu/src/data/rule_base.py
+++ ryu/src/data/rule_base.py
@@ -2,9 +2,9 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def rule_base(submission:pd.DataFrame, data:dict) -> pd.DataFrame:
+def rule_base(submission: pd.DataFrame, data: dict) -> pd.DataFrame:
     print("-----------------Start Rule Base-----------------")
-    for idx in tqdm(submission):
+    for idx in tqdm(range(len(submission))):
         if not submission["user_id"][idx] in data["unique_user_ids"] and not submission["isbn"][idx] in data["unique_isbns"]:
             submission.at[idx, "rating"] = data["train_mean_rating"]
         elif not submission["user_id"][idx] in data["unique_user_ids"]:
