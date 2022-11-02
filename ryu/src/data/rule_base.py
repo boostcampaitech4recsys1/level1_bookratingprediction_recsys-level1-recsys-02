4de119cf17d9fb7adac4d92347f2180e2f940784
ryubright <tmxkdd5790@gmail.com>
2022-11-02 16:29:37 +0900

    ADD rule base

---
+++ ryu/src/data/rule_base.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from tqdm import tqdm
+
+
+def rule_base(submission:pd.DataFrame, data:dict) -> pd.DataFrame:
+    print("-----------------Start Rule Base-----------------")
+    for idx in tqdm(submission):
+        if not submission["user_id"][idx] in data["unique_user_ids"] and not submission["isbn"][idx] in data["unique_isbns"]:
+            submission.at[idx, "rating"] = data["train_mean_rating"]
+        elif not submission["user_id"][idx] in data["unique_user_ids"]:
+            submission.at[idx, "rating"] = data["isbn2rating"][submission["isbn"][idx]]
+        elif not submission["isbn"][idx] in data["unique_isbns"]:
+            submission.at[idx, "rating"] = data["user2rating"][submission["user_id"][idx]]
+        else:
+            pass
+
+    return submission
